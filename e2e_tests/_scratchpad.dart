9e771e2b4b240971a2acd743be195c2b2fc2e44f
Star Ford <starluciaford@gmail.com>
2019-11-26 19:54:42 -0700

    random

--- e2e_tests/_scratchpad.dart
+++ e2e_tests/_scratchpad.dart
@@ -1,8 +1,30 @@
 import 'dart:io';
 
+class Color {
+  int _r;
+  Color.plain() {}
+  Color.fromRGB(int r, int g, int b) :
+    _r = r { //this assignment runs first
+    //body //this body runs third
+    print('3');
+  }
+}
+class AlphaColor extends Color {
+  int _a;
+  AlphaColor.fromRGBA(int r, int g, int b, int a) : 
+    _a = a,
+    super.fromRGB(r,g,b) { //this assignment runs second
+    //body //this body runs fourth
+    print('4');
+  }
+
+  AlphaColor.plain() : super.plain();
+
+}
 ///scratchpad/tests
 main() async {
-  stdin.readLineSync();
+  var c2 = AlphaColor.fromRGBA(1, 2, 3, 4);
+  //stdin.readLineSync();
 }
 
 
